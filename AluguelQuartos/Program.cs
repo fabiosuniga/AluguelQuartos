3d0465e34e9f345300ee0ba67e6a7f4d2c307f6a
fabiosuniga <fabiosuniga@hotmail.com>
2025-03-22 21:48:52 -0300

    Add project files.

---
+++ AluguelQuartos/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace AluguelQuartos {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Quartos[] quartos = new Quartos[10];
+            
+
+            Console.Write("Quantos estudantes serão adicionados: ");
+            int estudante = int.Parse(Console.ReadLine());
+            Console.WriteLine( );
+
+            for(int i = 0 ; i < estudante; i++) {
+                Console.Write($"Qual o nome do estudante {i + 1}: ");
+                string nome = Console.ReadLine();
+
+                Console.Write($"Qual quarto {nome} vai ficar: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                Console.Write($"Qual o aluguel do(a) {nome}: ");
+                double aluguel = double.Parse(Console.ReadLine());
+
+                quartos[numero] = new Quartos(nome, aluguel);
+
+                Console.WriteLine();
+            }
+
+            for(int i = 0; i < 10; i++) {
+                if (quartos[i] != null) {
+                    Console.Write($"Numero do quarto: {i}");
+                    quartos[i].DadosQuarto();
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
---
+++ AluguelQuartos/Quartos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluguelQuartos {
+    internal class Quartos {
+        public string Nome { get; set; }
+        public double Aluguel { get; set; }
+        public int Numero { get; set; }
+
+        public Quartos(string nome, double aluguel) {
+            Nome = nome;
+            Aluguel = aluguel;  
+        }
+
+        public void DadosQuarto() {
+            Console.Write($"\nO nome do estudante é: {Nome}\nAluguel: R${Aluguel.ToString("F2")}\n");
+        }
+    }
+}
